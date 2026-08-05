7239f1a9d3be71a7f4170e42e8d1e73f718a4f93
chorn <christopher.horn@tanium.com>
2026-08-05 10:17:19 -0400

    more lazyvim for nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -15,7 +15,11 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   spec = {
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { "LazyVim/LazyVim",
+      lazy = false,
+      import = "lazyvim.plugins",
+
+    },
     { 'nvim-treesitter/nvim-treesitter', lazy = false, build = ':TSUpdate' },
     { 'neovim/nvim-lspconfig', lazy = false, dependencies = { { "ms-jpq/coq_nvim", branch = "coq" }, { "ms-jpq/coq.artifacts", branch = "artifacts" }, { 'ms-jpq/coq.thirdparty', branch = "3p" }}},
     'nvim-tree/nvim-web-devicons',
@@ -31,47 +35,11 @@ require("lazy").setup({
     'wellle/context.vim',
     'junegunn/fzf.vim',
     'nvim-lualine/lualine.nvim',
-    },
-    'saghen/blink.cmp', {
-      dependencies = {
-        'saghen/blink.lib',
-        'rafamadriz/friendly-snippets',
-      },
-      build = function()
-        -- build the fuzzy matcher, optionally add a timeout to `pwait(timeout_ms)`
-        -- you can use `gb` in `:Lazy` to rebuild the plugin as needed
-        require('blink.cmp').build():pwait()
-      end,
-
-      ---@module 'blink.cmp'
-      ---@type blink.cmp.Config
-      opts = {
-        -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-        -- 'super-tab' for mappings similar to vscode (tab to accept)
-        -- 'enter' for enter to accept
-        -- 'none' for no mappings
-        -- All presets have the following mappings:
-        -- C-space: Open menu or open docs if already open
-        -- C-n/C-p or Up/Down: Select next/previous item
-        -- C-e: Hide menu
-        -- C-k: Toggle signature help (if signature.enabled = true)
-        -- See :h blink-cmp-config-keymap for defining your own keymap
-        keymap = { preset = 'default' },
-        -- (Default) Only show the documentation popup when manually triggered
-        completion = { documentation = { auto_show = true } },
-        -- (Default) list of enabled providers defined so that you can extend it
-        -- elsewhere in your config, without redefining it, due to `opts_extend`
-        sources = { default = { 'lsp', 'path', 'snippets', 'buffer' } },
-        -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-        -- You may use a lua implementation instead by using `implementation = "lua"`
-        -- See the fuzzy documentation for more information
-        fuzzy = { implementation = "rust" }
-      },
-    },
-
-  install = {
-    colorscheme = { 'retrobox' }
+    { "MeanderingProgrammer/render-markdown.nvim", enabled = false },
+    { "render-markdown.nvim", enabled = false },
   },
+  install = { colorscheme = { 'retrobox' }  },
+  checker = { enabled = true },
 })
 
 require("tree-sitter-manager").setup({
@@ -173,10 +141,11 @@ vim.opt.expandtab      = true
 -- vim.opt.maxmempattern  = 8192
 -- vim.opt.modeline       = true
 -- vim.opt.modelines      = 8
--- vim.opt.number         = true
--- vim.opt.numberwidth    = 6
+vim.opt.number         = true
+vim.opt.numberwidth    = 6
 -- vim.opt.preserveindent = true
 -- vim.opt.redrawtime     = 1000
+vim.opt.relativenumber = false
 -- vim.opt.report         = 0
 -- vim.opt.ruler          = true
 -- vim.opt.scrolljump     = 5
